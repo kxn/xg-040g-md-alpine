8aefc570bfc689dc02a78f550858d5b9f3646d6e
kxn <kangxn@gmail.com>
2025-11-14 16:50:47 +0800

    two versions

--- build.sh
+++ build.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-sudo mksquashfs rootfs rootfs-alpine.squashfs -comp xz -b 262144 -noappend -all-root;ubinize -o xg40-alpine.ubi -m 2048 -p 131072 -O 2048 ubinize.cfg
+sudo rm -rf rootfs
+sudo tar zxf rootfs.6.6.tar.gz
+sudo mksquashfs rootfs rootfs-alpine.squashfs -comp xz -b 262144 -noappend -all-root
+ubinize -o xg40-alpine-6.6.ubi -m 2048 -p 131072 -O 2048 ubinize.6.6.cfg
+sudo rm -rf rootfs
+sudo tar zxf rootfs.6.12.tar.gz
+sudo mksquashfs rootfs rootfs-alpine.squashfs -comp xz -b 262144 -noappend -all-root
+ubinize -o xg40-alpine-6.12.ubi -m 2048 -p 131072 -O 2048 ubinize.6.12.cfg
+
